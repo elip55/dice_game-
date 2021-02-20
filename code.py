599635e6157605062d4161e370f3bd6759fe083d
Eli Pacheco <eli.pacheco55@gmail.com>
2021-02-19 17:33:41 -0700

    Finally a decent game

--- code.py
+++ code.py
@@ -2,11 +2,15 @@ import random
 def main_code():
     min = 1
     max = 6
-    answer = input("Would you like to roll the dice?")
-    if answer == 'yes' or answer == 'Yes' or answer == 'y' or answer == 'Y':
+    roll = input("Press 'r' to roll the dice ")
+    if roll == 'r' or roll == 'R' or roll == 'roll' or roll == 'Roll':
         print(random.randint(min,max))
         print(random.randint(min,max))
-    if answer == 'no' or answer == 'No':
-        print("Thank you for visiting my program! ")
+    answer = input("Would you like to roll again?")
+    while answer == 'yes' or answer == 'Yes':
+        main_code()
+        break
+    else: 
+        print("Thank you for visiting my program!")
 
 main_code()
